d72fd4ee55744a9a194323bffc8368f04a92e7cd
S7701 <S7701@users.noreply.github.com>
2018-04-25 17:14:11 +0200

    Update micro-lisp

--- micro-lisp/micro-lisp.c
+++ micro-lisp/micro-lisp.c
@@ -12,7 +12,7 @@ typedef struct List {
   void * data;
 } List;
 
-static List *symbols = 0, *s_quote, *s_if, *s_lambda, *s_apply, *e_true;
+static List *symbols = 0, *s_quote, *s_if, *s_lambda, *s_macro, *s_apply, *e_true;
 static int look; /* look ahead character */
 
 #define SYMBOL_MAX  256
@@ -85,15 +85,15 @@ static void print_obj(List *ob, int head_of_list) {
   }
 }
 
-static List *fcons(List *a)    {  return cons(car(a), car(cdr(a)));  }
-static List *fcar(List *a)     {  return car(car(a));  }
-static List *fcdr(List *a)     {  return cdr(car(a));  }
-static List *feq(List *a)      {  return car(a) == car(cdr(a)) ? e_true : e_false;  }
-static List *fpair(List *a)    {  return is_pair(car(a))       ? e_true : e_false;  }
-static List *fatom(List *a)    {  return is_atom(car(a))       ? e_true : e_false;  }
-static List *fnull(List *a)    {  return car(a) == 0           ? e_true : e_false; }
-static List *freadobj(List *a) {  look = getchar(); gettoken(); return getobj();  }
-static List *fwriteobj(List *a){  print_obj(car(a), 1); puts(""); return e_true;  }
+static List *fcons(List *a)     {  return cons(car(a), car(cdr(a)));  }
+static List *fcar(List *a)      {  return car(car(a));  }
+static List *fcdr(List *a)      {  return cdr(car(a));  }
+static List *feq(List *a)       {  return car(a) == car(cdr(a)) ? e_true : e_false;  }
+static List *fpair(List *a)     {  return is_pair(car(a))       ? e_true : e_false;  }
+static List *fatom(List *a)     {  return is_atom(car(a))       ? e_true : e_false;  }
+static List *fnull(List *a)     {  return car(a) == 0           ? e_true : e_false; }
+static List *freadobj(List *a)  {  look = getchar(); gettoken(); return getobj();  }
+static List *fwriteobj(List *a) {  print_obj(car(a), 1); puts(""); return e_true;  }
 
 static List *eval(List *exp, List *env);
 
@@ -116,50 +116,55 @@ static List *eval(List *exp, List *env) {
     for ( ; env != 0; env = cdr(env) )
       if (exp == car(car(env)))  return car(cdr(car(env)));
     return 0;
-  } else if (is_atom( car (exp))) { /* special forms */
+  } else if (is_atom(car(exp))) { /* special forms */
     if (car(exp) == s_quote) {
       return car(cdr(exp));
     } else if (car(exp) == s_if) {
-      if (eval (car(cdr(exp)), env) != 0)
-        return eval (car(cdr(cdr(exp))), env);
+      if (eval(car(cdr(exp)), env) != 0)
+        return eval(car(cdr(cdr(exp))), env);
       else
-        return eval (car(cdr(cdr(cdr(exp)))), env);
-    } else if (car(exp) == s_lambda) {
+        return eval(car(cdr(cdr(cdr(exp)))), env);
+    } else if (car(exp) == s_lambda || car(exp) == s_macro) {
       return exp; /* TODO: create a closure and capture free vars */
     } else if (car(exp) == s_apply) { /* apply function to list */
-      List *args = evlist (cdr(cdr(exp)), env);
+      List *args = evlist(cdr(cdr(exp)), env);
       args = car(args); /* assumes one argument and that it is a list */
-      return apply_primitive( eval(car(cdr(exp)), env), args);
+      return apply_primitive(eval(car(cdr(exp)), env), args);
     } else { /* function call */
-      List *primop = eval (car(exp), env);
-      if (is_pair(primop)) { /* user defined lambda, arg list eval happens in binding  below */
-        return eval( cons(primop, cdr(exp)), env );
+      List *primop = eval(car(exp), env);
+      if (is_pair(primop)) { /* user defined lambda or macro, arg list eval happens in binding  below */
+        return eval(cons(primop, cdr(exp)), env );
       } else if (primop) { /* built-in primitive */
         return apply_primitive(primop, evlist(cdr(exp), env));
       }
     }
   } else if (car(car(exp)) == s_lambda) { /* should be a lambda, bind names into env and eval body */
     List *extenv = env, *names = car(cdr(car(exp))), *vars = cdr(exp);
-    for (  ; names ; names = cdr(names), vars = cdr(vars) )
-      extenv = cons (cons(car(names),  cons(eval (car(vars), env), 0)), extenv);
-    return eval (car(cdr(cdr(car(exp)))), extenv);
+    for ( ; names; names = cdr(names), vars = cdr(vars) )
+      extenv = cons(cons(car(names), cons(eval(car(vars), env), 0)), extenv);
+    return eval(car(cdr(cdr(car(exp)))), extenv);
+  } else if (car(car(exp)) == s_macro) { /* should be a macro, bind names into env and eval body */
+    List *extenv = env, *names = car(cdr(car(exp))), *vars = cdr(exp);
+    for ( ; names; names = cdr(names), vars = cdr(vars) )
+      extenv = cons(cons(car(names), cons(car(vars), 0)), extenv);
+    return eval(car(cdr(cdr(car(exp)))), extenv);
   }
-  puts("cannot evaluate expression");
+  puts("cannot evaluate expression: "); print_obj(exp, 1);
   return 0;
 }
 
 int main(int argc, char *argv[]) {
-  List *env = cons (cons(intern("car"), cons((void *)fcar, 0)),
-              cons (cons(intern("cdr"), cons((void *)fcdr, 0)),
-              cons (cons(intern("cons"), cons((void *)fcons, 0)),
-              cons (cons(intern("eq?"), cons((void *)feq, 0)),
-              cons (cons(intern("pair?"), cons((void *)fpair, 0)),
-              cons (cons(intern("symbol?"), cons((void *)fatom, 0)),
-              cons (cons(intern("null?"), cons((void *)fnull, 0)),
-              cons (cons(intern("read"), cons((void *)freadobj, 0)),
-              cons (cons(intern("write"), cons((void *)fwriteobj, 0)),
-              cons (cons(intern("null"), cons(0,0)), 0))))))))));
-  s_quote = intern("quote"); s_if = intern("if"); s_lambda = intern("lambda"); s_apply = intern("apply");
+  List *env = cons(cons(intern("car"), cons((void *)fcar, 0)),
+              cons(cons(intern("cdr"), cons((void *)fcdr, 0)),
+              cons(cons(intern("cons"), cons((void *)fcons, 0)),
+              cons(cons(intern("eq?"), cons((void *)feq, 0)),
+              cons(cons(intern("pair?"), cons((void *)fpair, 0)),
+              cons(cons(intern("symbol?"), cons((void *)fatom, 0)),
+              cons(cons(intern("null?"), cons((void *)fnull, 0)),
+              cons(cons(intern("read"), cons((void *)freadobj, 0)),
+              cons(cons(intern("write"), cons((void *)fwriteobj, 0)),
+              cons(cons(intern("null"), cons(0,0)), 0))))))))));
+  s_quote = intern("quote"); s_if = intern("if"); s_lambda = intern("lambda"); s_macro = intern("macro"); s_apply = intern("apply");
   e_true = cons( s_quote, cons( intern("t"), 0));
   for (;;) {
     look = getchar();
