a0727566e95ee87424f435d34f27262f11603c4b
S7701 <s7701@github.com>
2023-02-23 15:23:08 +0100

    Update prolog.cpp

--- prolog/prolog.cpp
+++ prolog/prolog.cpp
@@ -1,19 +1,22 @@
 #include <cassert>
+#include <cstring>
 #include <iostream>
 #include <list>
-#include <string>
 
 using namespace std;
 
-bool debug = true;
+bool debug = false;
 
-class Atom;
+void indent(unsigned int n) {
+  for (unsigned int i = 0; i < n; ++i) cout << "    ";
+}
+
 class CompoundTerm;
 class Variable;
 
 class Term {
 public: // statics
-  static void Print(const list<Term*> &l, const string &s) {
+  static void Print(const list<Term*> &l, const char *s) {
     for (auto it = l.begin();;) {
       (*it)->print();
       if (++it == l.end()) break;
@@ -23,48 +26,29 @@ public: // statics
 public:
   virtual void print() const = 0;
   virtual bool unify(Term *t) = 0;
-  virtual bool unifyAtom(Atom *a) { return false; }
   virtual bool unifyCompoundTerm(CompoundTerm *ct) { return false; }
   virtual bool unifyVariable(Variable *v) { return false; }
 };
 
-class Atom: public Term { // name.
-private:
-  string txt;
-public:
-  Atom(const string &s): txt(s) {}
-  const string &getTxt() const { return txt; }
-  void print() const { cout << txt; }
-  bool unify(Term *t) { return t->unifyAtom(this); }
-  virtual bool unifyAtom(Atom *a) {
-    if (txt == a->txt)
-      return true;
-    return false;
-  }
-  bool operator == (Atom* other) { return txt == other->txt; }
-};
-
-Atom textless("");
-
 class CompoundTerm: public Term { // functor(arg0, arg1 ... argN).
 private:
-  Atom *functor;
+  const char *functor;
   unsigned int arity;
   list<Term*> args;
 public:
-  CompoundTerm(Atom *f): functor(f), arity(0) {}
+  CompoundTerm(const char *f): functor(f), arity(0) {}
   void addArg(Term *arg) { args.push_back(arg); ++arity; }
   void print() const {
-    functor->print();
-    cout << "(";
+    cout << functor;
     if (!args.empty()) {
+      cout << "(";
       Term::Print(args, ", ");
+      cout << ")";
     }
-    cout << ")";
   };
   bool unify(Term *t) { return t->unifyCompoundTerm(this); }
   bool unifyCompoundTerm(CompoundTerm *ct) {
-    if ((functor != ct->functor) || (arity != ct->arity))
+    if (functor != ct->functor || arity != ct->arity)
       return false;
     auto it1 = args.begin();
     auto it2 = ct->args.begin();
@@ -80,40 +64,36 @@ private: // statics
   static unsigned int count;
   static list<Variable*> trail;
 public: // statics
-  static Variable *GetTrailhead() { return trail.back(); }
+  static Variable *GetTrailhead() { return trail.front(); }
   static void Rewind(Variable *to) {
-    Variable *v = trail.back();
+    Variable *v = trail.front();
     while (v != to) {
       v->reset();
-      trail.pop_back();
-      v = trail.back();
+      trail.pop_front();
+      v = trail.front();
     }
   }
 private:
-  Atom *name;
+  const char *name;
   unsigned int id;
   Term *term;
 public:
-  Variable(): name(&textless), id(++count), term(NULL) {}
-  Variable(Atom *n): name(n), id(++count), term(NULL) {}
-  const string &getName() const { return name->getTxt(); }
+  Variable(): name(""), id(++count), term(NULL) {}
+  Variable(const char *n): name(n), id(++count), term(NULL) {}
+  const char *getName() const { return name; }
   void print() const {
-    const string &n = name->getTxt();
     if (term != NULL)
       term->print();
-    else if (!n.empty())
-      cout << "?" << n << "#" << id;
     else
-      cout << "#" << id;
+      cout << "?" << name << "#" << id;
   };
   bool unify(Term *t) {
     if (term != NULL)
       return term->unify(t);
-    trail.push_back(this);
+    trail.push_front(this);
     term = t;
     return true;
   }
-  bool unifyAtom(Atom *a) { return unify(a); }
   bool unifyCompoundTerm(CompoundTerm *ct) { return unify(ct); }
   bool unifyVariable(Variable *v) { return unify(v); }
   void reset() { term = NULL; }
@@ -122,128 +102,110 @@ public:
 unsigned int Variable::count = 0;
 list<Variable*> Variable::trail;
 
-class Clause { // head :- goals
+class Clause { // head :- body
 private:
   Term *head;
-  list<Term*> goals; // empty for facts
+  list<Term*> body; // empty for facts
 public:
   Clause(Term *h) : head(h) {}
   Term *getHead() const { return head; }
-  list<Term*> &getGoals() { return goals; }
-  void addGoal(Term *t) { goals.push_back(t); }
+  list<Term*> &getBody() { return body; }
+  void addGoal(Term *t) { body.push_back(t); }
   void print() const {
     head->print();
     cout << " :- ";
-    if (goals.empty())
+    if (body.empty())
       cout << "true";
     else
-      Term::Print(goals, ", ");
+      Term::Print(body, ", ");
   }
 };
-
-void indent(unsigned int n) {
-  for (unsigned int i = 0; i < n; ++i) cout << "    ";
-}
 
 class Program {
 private:
   list<Clause*> clauses;
 public:
   void addClause(Clause *c) { clauses.push_back(c); }
-  bool solve(Term *q, list<Variable*> &vl, unsigned int level = 0) {
-    bool solved = false;
-    indent(level);
-    if (debug) {
-      cout << "solve@"  << level << ": ";
-      q->print();
-      cout << "\n";
-    }
+  int solve(list<Term*> &ql, list<Variable*> &vl, unsigned int level = 0) {
+    int solved = 0;
+    Term *q = ql.front();
+    ql.pop_front();
+    if (debug) { indent(level); cout << "solve@"  << level << ": "; q->print(); cout << endl; }
     for (auto it = clauses.begin(); it != clauses.end(); ++it) {
-      Variable *trailhead = Variable::GetTrailhead();
+      auto trailhead = Variable::GetTrailhead();
       Clause *c = (*it);
       Term *head = c->getHead();
-      if (debug) {
-        indent(level);
-        cout << "  try: ";
-        c->print();
-        cout << "\n";
-      }
+      if (debug) { indent(level); cout << "  try: "; c->print(); cout << endl; }
       if (q->unify(head)) {
-        if (debug) {
-          indent(level);
-          cout << "  match\n";
-        }
-        list<Term*> &goals = c->getGoals();
+        if (debug) { indent(level); cout << "  MATCH" << endl; }
+        list<Term*> goals(c->getBody()); // copy goals from clause
+        goals.insert(goals.end(), ql.begin(), ql.end()); // append ql to goals
         if (goals.empty()) {
-          solved = true;
-          if (vl.empty()) {
-            if (level == 0)
-              cout << "true";
-          }
-          else {
+          ++solved;
+          if (!vl.empty()) {
             for (auto it = vl.begin(); it != vl.end(); ++it) {
               Variable* v = (*it);
               cout << v->getName() << " = ";
               v->print();
-              cout << "\n";
+              cout << endl;
             }
-            cout << "\n";
+            cout << endl;
           }
         }
-        else if(solve(goals, vl, level+1))
-          solved = true;
+        else {
+          solved += solve(goals, vl, level+1);
+        }
       }
-      else if (debug) {
-        indent(level);
-        cout << "  no match\n";
+      else {
+        if (debug) { indent(level); cout << "  no match" << endl; }
       }
       Variable::Rewind(trailhead);
     }
-    if (level == 0)
-      cout << (solved ? "true\n" : "false\n");
+    if (level == 0) {
+      if (solved) {
+        cout << "true";
+        if (solved > 1) cout << " (" << solved << " solutions)";
+        cout << endl;
+      }
+      else
+        cout << "false" << endl;
+    }
     return solved;
   }
-  bool solve(list<Term*> &ql, list<Variable*> &vl, unsigned int level = 0) {
-    bool solved = false;
-    indent(level);
-    cout << "solve@"  << level << ": ";
-    Term::Print(ql, ", ");
-    cout << "\n";
-    for (auto it = ql.begin(); it != ql.end(); ++it) {
-      Term* q = (*it);
-      if (solve(q, vl, level+1))
-        solved = true;
-    }
-    if (level == 0 && !solved)
-      cout << (solved ? "true\n" : "false\n");
-    return solved;
+  int solve(Term *q, list<Variable*> &vl) {
+    list<Term*> ql;
+    ql.push_back(q);
+    return solve(ql, vl);
   }
 };
 
 // program: (clause | query)+.
-// clause: term [":-" term (',' term)*] '.'.
-// term: atom | variable | compound-term.
-// atom: (printable)+ | '\'' (printable | whitespcae)+ '\''.
-// variable: '?' atom.
-// compound-term: atom '(' [term (',' term)*] ')'.
-// query: "?-" compound-term '.'.
+// clause: head [":-" body] '.'.
+// query: "?-" goals '.'.
+// head: term.
+// body: goals.
+// goals: term (',' term)*.
+// term: variable | compound-term.
+// variable: '?' name.
+// compound-term: name '(' [term (',' term)*] ')'.
+// name: (printable)+ | '\'' (printable | whitespcae)+ '\''.
 
-int main(int argc, const char* const argv[]) {
-  Atom *aGerd = new Atom("Gerd");
-  Atom *aLaura = new Atom("Laura");
-  Atom *aKH = new Atom("Karl-Heinz");
-  Atom *aDoris = new Atom("Doris");
-  Atom *aMike = new Atom("Mike");
-  Atom *aNicki = new Atom("Nicki");
-  Atom *aNico = new Atom("Nico");
-  Atom *aLuca = new Atom("Luca");
-  Atom *aGrandchildGrandparent = new Atom("grandchild-grandparent");
-  Atom *aChildParent = new Atom("child-parent");
-  Atom *aGrandchild = new Atom("grandchild");
-  Atom *aChild = new Atom("child");
-  Atom *aParent = new Atom("parent");
-  Atom *aGrandparent = new Atom("grandparent");
-  Atom *aX = new Atom("x");
+void sample_test_program() {
+  Term *tGerd = new CompoundTerm("Gerd");
+  Term *tLaura = new CompoundTerm("Laura");
+  Term *tKarlHeinz = new CompoundTerm("Karl-Heinz");
+  Term *tDoris = new CompoundTerm("Doris");
+  Term *tMike = new CompoundTerm("Mike");
+  Term *tNicki = new CompoundTerm("Nicki");
+  Term *tNico = new CompoundTerm("Nico");
+  Term *tLuca = new CompoundTerm("Luca");
+  const char *aGrandchildGrandparent = "grandchild-grandparent";
+  const char *aChildParent = "child-parent";
+  const char *aGrandchild = "grandchild";
+  const char *aChild = "child";
+  const char *aParent = "parent";
+  const char *aGrandparent = "grandparent";
+  const char *aX = "x";
 
   Program *p = new Program();
   Clause *c;
@@ -254,35 +216,35 @@ int main(int argc, const char* const argv[]) {
   // Facts:
 
   // child-parent(Mike, Gerd).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aMike); ct->addArg(aGerd);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tMike); ct->addArg(tGerd);
   p->addClause(new Clause(ct));
 
   // child-parent(Mike, Laura).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aMike); ct->addArg(aLaura);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tMike); ct->addArg(tLaura);
   p->addClause(new Clause(ct));
 
   // child-parent(Nicki, Karl-Heinz).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aNicki); ct->addArg(aKH);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tNicki); ct->addArg(tKarlHeinz);
   p->addClause(new Clause(ct));
 
   // child-parent(Nicki, Doris).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aNicki); ct->addArg(aDoris);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tNicki); ct->addArg(tDoris);
   p->addClause(new Clause(ct));
 
   // child-parent(Nico, Mike).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aNico); ct->addArg(aMike);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tNico); ct->addArg(tMike);
   p->addClause(new Clause(ct));
 
   // child-parent(Luca, Mike).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aLuca); ct->addArg(aMike);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tLuca); ct->addArg(tMike);
   p->addClause(new Clause(ct));
 
   // child-parent(Nico, Nicki).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aNico); ct->addArg(aNicki);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tNico); ct->addArg(tNicki);
   p->addClause(new Clause(ct));
 
   // child-parent(Luca, Nicki).
-  ct = new CompoundTerm(aChildParent); ct->addArg(aLuca); ct->addArg(aNicki);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tLuca); ct->addArg(tNicki);
   p->addClause(new Clause(ct));
 
   // Rules:
@@ -302,45 +264,51 @@ int main(int argc, const char* const argv[]) {
   // Queries:
 
   vl.clear();
-  cout << "?- child-parent(?child, Nicki).\n";
+  cout << "?- child-parent(?child, Nicki)." << endl;
   v1 = new Variable(aChild); vl.push_back(v1);
-  ct = new CompoundTerm(aChildParent); ct->addArg(v1); ct->addArg(aNicki);
+  ct = new CompoundTerm(aChildParent); ct->addArg(v1); ct->addArg(tNicki);
   p->solve(ct, vl);
 
   vl.clear();
-  cout << "\n?- child-parent(?child, Luca).\n";
+  cout << "\n?- child-parent(?child, Luca)." << endl;
   v1 = new Variable(aChild); vl.push_back(v1);
-  ct = new CompoundTerm(aChildParent); ct->addArg(v1); ct->addArg(aLuca);
+  ct = new CompoundTerm(aChildParent); ct->addArg(v1); ct->addArg(tLuca);
   p->solve(ct, vl);
 
   vl.clear();
-  cout << "\n?- child-parent(?child, ?parent).\n";
+  cout << "\n?- child-parent(?child, ?parent)." << endl;
   v1 = new Variable(aChild); vl.push_back(v1);
   v2 = new Variable(aParent); vl.push_back(v2);
   ct = new CompoundTerm(aChildParent); ct->addArg(v1); ct->addArg(v2);
   p->solve(ct, vl);
 
-  cout << "\n?- child-parent(Nico, Mike).\n";
+  cout << "\n?- child-parent(Nico, Mike)." << endl;
   vl.clear();
-  ct = new CompoundTerm(aChildParent); ct->addArg(aNico); ct->addArg(aMike);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tNico); ct->addArg(tMike);
   p->solve(ct, vl);
 
-  cout << "\n?- child-parent(Luca, Gerd).\n";
+  cout << "\n?- child-parent(Luca, Gerd)." << endl;
   vl.clear();
-  ct = new CompoundTerm(aChildParent); ct->addArg(aLuca); ct->addArg(aGerd);
+  ct = new CompoundTerm(aChildParent); ct->addArg(tLuca); ct->addArg(tGerd);
   p->solve(ct, vl);
 
-  cout << "\n?- grandchild-grandparent(Luca, Gerd).\n";
+  cout << "\n?- grandchild-grandparent(Luca, Gerd)." << endl;
   vl.clear();
-  ct = new CompoundTerm(aGrandchildGrandparent); ct->addArg(aLuca); ct->addArg(aGerd);
+  ct = new CompoundTerm(aGrandchildGrandparent); ct->addArg(tLuca); ct->addArg(tGerd);
   p->solve(ct, vl);
 
   vl.clear();
-  cout << "\n?- grandchild-grandparent(?grandchild, ?grandparent).\n";
+  cout << "\n?- grandchild-grandparent(?grandchild, ?grandparent)." << endl;
   v1 = new Variable(aGrandchild); vl.push_back(v1);
   v2 = new Variable(aGrandparent); vl.push_back(v2);
   ct = new CompoundTerm(aGrandchildGrandparent); ct->addArg(v1); ct->addArg(v2);
   p->solve(ct, vl);
+}
+
+int main(int argc, const char* const argv[]) {
+  if (argc > 1 && strcmp(argv[1], "-d") == 0) debug = true;
+
+  sample_test_program();
 
   return 0;
 }
