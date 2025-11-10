725aa85c97bbb366559cb5ddc55c25d79377dfa4
S7701 <s7701@github.com>
2025-11-10 08:47:16 +0100

    Update micro-lisp

--- micro-lisp/micro-lisp.c
+++ micro-lisp/micro-lisp.c
@@ -7,167 +7,168 @@
 
 #define debug(m, e) printf("%s:%d: %s: ", __FILE__, __LINE__, m); print_obj(e, 1); puts("");
 
-typedef struct Pair {
-  void *left;
-  struct Pair *right;
-} Pair;
+typedef struct List {
+  void *head;
+  struct List *tail;
+} List;
 
-static Pair *symbols = 0, *s_quote, *s_if, *s_lambda, *s_macro, *s_apply, *e_true, *e_false = 0;
-static int look; /* look ahead character */
+static List *symbols = 0, *sym_quote, *sym_if, *sym_lambda, *sym_macro, *sym_apply, *exp_true, *exp_false = 0;
 
 #define SYMBOL_MAX 256
 
+static int look; /* look ahead character */
 static char token[SYMBOL_MAX]; /* token */
+
 static int is_space(char x)  { return x == ' ' || x == '\t' || x == '\n'; }
 static int is_parens(char x) { return x == '(' || x == ')'; }
 
-static void get_token() {
-  int index = 0;
+static void read_token() {
+  int len = 0;
   while(is_space(look)) { look = getchar(); }
   if (is_parens(look)) {
-    token[index++] = look;  look = getchar();
+    token[len++] = look;  look = getchar();
   } else {
-    while(index < SYMBOL_MAX - 1 && look != EOF && !is_space(look) && !is_parens(look)) {
-      token[index++] = look;  look = getchar();
+    while(len < SYMBOL_MAX - 1 && look != EOF && !is_space(look) && !is_parens(look)) {
+      token[len++] = look;  look = getchar();
     }
   }
-  token[index] = '\0';
+  token[len] = '\0';
 }
 
-#define is_pair(x) (((uintptr_t)x & 0x1) == 0x1)  /* tag pointer to pair with 0x1 (alignment dependent)*/
-#define is_atom(x) (((uintptr_t)x & 0x1) == 0x0)
-#define untag(x)   ((uintptr_t) x & ~0x1)
-#define tag(x)     ((uintptr_t) x | 0x1)
-#define left(x)    (((Pair*)untag(x))->left)
-#define right(x)   (((Pair*)untag(x))->right)
+#define is_list(x)  (((uintptr_t)x & 0x1) == 0x1)  /* tag pointer to list with 0x1 (alignment dependent)*/
+#define is_atom(x)  (((uintptr_t)x & 0x1) == 0x0)
+#define untag(x)    ((uintptr_t) x & ~0x1)
+#define tag(x)      ((uintptr_t) x | 0x1)
+#define get_head(x) (((List*)untag(x))->head)
+#define get_tail(x) (((List*)untag(x))->tail)
 
-static Pair *pair(void *_left, void *_right) {
-  Pair *_pair = calloc(1, sizeof (Pair));
-  _pair->left = _left;
-  _pair->right = _right;
-  return (Pair*) tag(_pair);
+static List *new_list(void *h, void *t) {
+  List *p = calloc(1, sizeof (List));
+  p->head = h;
+  p->tail = t;
+  return (List*) tag(p);
 }
 
-static void *intern(char *sym) {
-  Pair *_pair;
-  for (_pair = symbols; _pair ; _pair = right(_pair))
-    if (strncmp(sym, (char*) left(_pair), SYMBOL_MAX)==0) return left(_pair);
-  symbols = pair(strdup(sym), symbols);
-  return left(symbols);
+static void *get_sym(char *str) {
+  List *p;
+  for (p = symbols; p ; p = get_tail(p))
+    if (strncmp(str, (char*) get_head(p), SYMBOL_MAX)==0) return get_head(p);
+  symbols = new_list(strdup(str), symbols);
+  return get_head(symbols);
 }
 
-static Pair *get_list();
+static List *read_list();
 
-static void * get_obj() {
-  if (token[0] == '(') return get_list();
-  return intern(token);
+static void *read_obj() {
+  if (token[0] == '(') return read_list();
+  return get_sym(token);
 }
 
-static Pair *get_list() {
-  Pair *tmp;
-  get_token();
+static List *read_list() {
+  List *p;
+  read_token();
   if (token[0] == ')') return 0;
-  tmp = get_obj();
-  return pair(tmp, get_list());
+  p = read_obj();
+  return new_list(p, read_list());
 }
 
-static void print_obj(Pair *obj, int head_of_list) {
-  if (!is_pair(obj) ) {
+static void print_obj(List *obj, int head_of_list) {
+  if (!is_list(obj) ) {
     printf("%s", obj ? (char*) obj : "null" );
   } else {
     if (head_of_list) printf("(");
-    print_obj(left(obj), 1);
-    if (right(obj) != 0) {
+    print_obj(get_head(obj), 1);
+    if (get_tail(obj) != 0) {
       printf(" ");
-      print_obj(right(obj), 0);
+      print_obj(get_tail(obj), 0);
     } else  printf(")");
   }
 }
 
-static Pair *eval(Pair *exp, Pair *env);
+static List *eval(List *exp, List *env);
 
-static Pair *eval_list(Pair *list, Pair *env) {
+static List *eval_list(List *list, List *env) {
   /* http://cslibrary.stanford.edu/105/LinkedListProblems.pdf */
-  Pair *head = 0, **args = &head;
-  for (; list ; list = right(list)) {
-    *args = pair(eval(left(list), env) , 0);
-    args = &((Pair*) untag(*args))->right;
+  List *head = 0, **args = &head;
+  for (; list ; list = get_tail(list)) {
+    *args = new_list(eval(get_head(list), env) , 0);
+    args = &((List*) untag(*args))->tail;
   }
   return head;
 }
 
-static Pair *apply_primitive(void *primfn, Pair *args) {
-  return ((Pair*(*) (Pair*)) primfn) (args);
+static List *apply_primitive(void *primfn, List *args) {
+  return ((List*(*) (List*)) primfn) (args);
 }
 
-static Pair *eval(Pair *exp, Pair *env) {
+static List *eval(List *exp, List *env) {
   if (is_atom(exp) ) {
-    for (; env != 0; env = right(env))
-      if (exp == left(left(env)))  return left(right(left(env)));
+    for (; env != 0; env = get_tail(env))
+      if (exp == get_head(get_head(env)))  return get_head(get_tail(get_head(env)));
     return 0;
-  } else if (is_atom(left(exp))) { /* special forms */
-    if (left(exp) == s_quote) {
-      return left(right(exp));
-    } else if (left(exp) == s_if) {
-      if (eval(left(right(exp)), env) != 0)
-        return eval(left(right(right(exp))), env);
+  } else if (is_atom(get_head(exp))) { /* special forms */
+    if (get_head(exp) == sym_quote) {
+      return get_head(get_tail(exp));
+    } else if (get_head(exp) == sym_if) {
+      if (eval(get_head(get_tail(exp)), env) != 0)
+        return eval(get_head(get_tail(get_tail(exp))), env);
       else
-        return eval(left(right(right(right(exp)))), env);
-    } else if (left(exp) == s_lambda || left(exp) == s_macro) {
+        return eval(get_head(get_tail(get_tail(get_tail(exp)))), env);
+    } else if (get_head(exp) == sym_lambda || get_head(exp) == sym_macro) {
       return exp; /* TODO: create a closure and capture free vars */
-    } else if (left(exp) == s_apply) { /* apply function to list */
-      Pair *args = eval_list(right(right(exp)), env);
-      args = left(args); /* assumes one argument and that it is a list */
-      return apply_primitive(eval(left(right(exp)), env), args);
+    } else if (get_head(exp) == sym_apply) { /* apply function to list */
+      List *args = eval_list(get_tail(get_tail(exp)), env);
+      args = get_head(args); /* assumes one argument and that it is a list */
+      return apply_primitive(eval(get_head(get_tail(exp)), env), args);
     } else { /* function call */
-      Pair *primop = eval(left(exp), env);
-      if (is_pair(primop)) { /* user defined lambda or macro, arg list eval happens in binding  below */
-        return eval(pair(primop, right(exp)), env );
+      List *primop = eval(get_head(exp), env);
+      if (is_list(primop)) { /* user defined lambda or macro, arg list eval happens in binding  below */
+        return eval(new_list(primop, get_tail(exp)), env );
       } else if (primop) { /* built-in primitive */
-        return apply_primitive(primop, eval_list(right(exp), env));
+        return apply_primitive(primop, eval_list(get_tail(exp), env));
       }
     }
-  } else if (left(left(exp)) == s_lambda) { /* should be a lambda, bind names into env and eval body */
-    Pair *extenv = env, *names = left(right(left(exp))), *vars = right(exp);
-    for (; names; names = right(names), vars = right(vars))
-      extenv = pair(pair(left(names), pair(eval(left(vars), env), 0)), extenv);
-    return eval(left(right(right(left(exp)))), extenv);
-  } else if (left(left(exp)) == s_macro) { /* should be a macro, bind names into env and eval body */
-    Pair *extenv = env, *names = left(right(left(exp))), *vars = right(exp);
-    for (; names; names = right(names), vars = right(vars))
-      extenv = pair(pair(left(names), pair(left(vars), 0)), extenv);
-    return eval(left(right(right(left(exp)))), extenv);
+  } else if (get_head(get_head(exp)) == sym_lambda) { /* should be a lambda, bind names into env and eval body */
+    List *extenv = env, *names = get_head(get_tail(get_head(exp))), *vars = get_tail(exp);
+    for (; names; names = get_tail(names), vars = get_tail(vars))
+      extenv = new_list(new_list(get_head(names), new_list(eval(get_head(vars), env), 0)), extenv);
+    return eval(get_head(get_tail(get_tail(get_head(exp)))), extenv);
+  } else if (get_head(get_head(exp)) == sym_macro) { /* should be a macro, bind names into env and eval body */
+    List *extenv = env, *names = get_head(get_tail(get_head(exp))), *vars = get_tail(exp);
+    for (; names; names = get_tail(names), vars = get_tail(vars))
+      extenv = new_list(new_list(get_head(names), new_list(get_head(vars), 0)), extenv);
+    return eval(get_head(get_tail(get_tail(get_head(exp)))), extenv);
   }
   puts("cannot evaluate expression: "); print_obj(exp, 1);
   return 0;
 }
 
-static Pair *f_left(Pair *a)     { return left(left(a)); }
-static Pair *f_right(Pair *a)    { return right(left(a)); }
-static Pair *f_pair(Pair *a)     { return pair(left(a), left(right(a))); }
-static Pair *f_eq(Pair *a)       { return left(a) == left(right(a)) ? e_true : e_false; }
-static Pair *f_pair(Pair *a)     { return is_pair(left(a)) ? e_true : e_false; }
-static Pair *f_atom(Pair *a)     { return is_atom(left(a)) ? e_true : e_false; }
-static Pair *f_null(Pair *a)     { return left(a) == 0     ? e_true : e_false; }
-static Pair *f_readobj(Pair *a)  { look = getchar(); get_token(); return get_obj(); }
-static Pair *f_writeobj(Pair *a) { print_obj(left(a), 1); puts(""); return e_true; }
+static List *fn_head(List *a)    { return get_head(get_head(a)); }
+static List *fn_tail(List *a)    { return get_tail(get_head(a)); }
+static List *fn_list(List *a)    { return new_list(get_head(a), get_head(get_tail(a))); }
+static List *fn_is_eq(List *a)   { return get_head(a) == get_head(get_tail(a)) ? exp_true : exp_false; }
+static List *fn_is_list(List *a) { return is_list(get_head(a)) ? exp_true : exp_false; }
+static List *fn_is_atom(List *a) { return is_atom(get_head(a)) ? exp_true : exp_false; }
+static List *fn_is_null(List *a) { return get_head(a) == 0     ? exp_true : exp_false; }
+static List *fn_read(List *a)    { look = getchar(); read_token(); return read_obj(); }
+static List *fn_write(List *a)   { print_obj(get_head(a), 1); puts(""); return exp_true; }
 
 int main(int argc, char *argv[]) {
-  Pair *env = pair(pair(intern("left"), pair((void *)f_left, 0)),
-              pair(pair(intern("right"), pair((void *)f_right, 0)),
-              pair(pair(intern("pair"), pair((void *)f_pair, 0)),
-              pair(pair(intern("eq?"), pair((void *)f_eq, 0)),
-              pair(pair(intern("pair?"), pair((void *)f_pair, 0)),
-              pair(pair(intern("symbol?"), pair((void *)f_atom, 0)),
-              pair(pair(intern("null?"), pair((void *)f_null, 0)),
-              pair(pair(intern("read"), pair((void *)f_readobj, 0)),
-              pair(pair(intern("write"), pair((void *)f_writeobj, 0)),
-              pair(pair(intern("null"), pair(0,0)), 0))))))))));
-  s_quote = intern("quote"); s_if = intern("if"); s_lambda = intern("lambda"); s_macro = intern("macro"); s_apply = intern("apply");
-  e_true = pair(s_quote, pair( intern("t"), 0));
+  List *env = new_list(new_list(get_sym("head"),  new_list((void *)fn_head, 0)),
+              new_list(new_list(get_sym("tail"),  new_list((void *)fn_tail, 0)),
+              new_list(new_list(get_sym("list"),  new_list((void *)fn_list, 0)),
+              new_list(new_list(get_sym("eq?"),   new_list((void *)fn_is_eq, 0)),
+              new_list(new_list(get_sym("list?"), new_list((void *)fn_is_list, 0)),
+              new_list(new_list(get_sym("atom?"), new_list((void *)fn_is_atom, 0)),
+              new_list(new_list(get_sym("null?"), new_list((void *)fn_is_null, 0)),
+              new_list(new_list(get_sym("read"),  new_list((void *)fn_read, 0)),
+              new_list(new_list(get_sym("write"), new_list((void *)fn_write, 0)),
+              new_list(new_list(get_sym("null"),  new_list(0,0)), 0))))))))));
+  sym_quote = get_sym("quote"); sym_if = get_sym("if"); sym_lambda = get_sym("lambda"); sym_macro = get_sym("macro"); sym_apply = get_sym("apply");
+  exp_true = new_list(sym_quote, new_list(get_sym("t"), 0));
   look = getchar();
-  get_token();
-  print_obj(eval(get_obj(), env), 1);
+  read_token();
+  print_obj(eval(read_obj(), env), 1);
   printf("\n");
   return 0;
 }
--- micro-lisp/mlisp89.c
+++ micro-lisp/mlisp89.c
@@ -1,18 +1,21 @@
 /* micro lisp (C) A. Carl Douglas */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define debug(m,e) printf("%s:%d: %s:",__FILE__,__LINE__,m); print_obj(e,1); puts("");
+
 typedef struct Object {
-  enum { _Symbol, _Pair, _Primitive, _Closure } tag;
+  enum { _List, _Symbol, _Primitive, _Closure } tag;
   union {
+    struct List {
+      struct Object * head;
+      struct Object * tail;
+    } list;
     char * string;
     struct Object * (*pfn) (struct Object *);
-    struct List {
-      struct Object * next;
-      struct Object * data;
-    } pair;
     struct Closure {
       struct Object * params;
       struct Object * body;
@@ -20,54 +23,59 @@ typedef struct Object {
     } closure;
   } value;
 } Object;
-Object * symbols = 0;
+
+static Object *symbols = 0, *sym_quote, *sym_if, *sym_lambda, *sym_apply, *exp_true, *exp_false = 0;
+
+#define SYMBOL_MAX 256
+
 static int look; /* look ahead character */
-#define SYMBOL_MAX  32
-static char token[SYMBOL_MAX]; /* token */
-int is_space(char x)  { return x == ' ' || x == '\n'; }
+static char token[SYMBOL_MAX+1]; /* token */
+
+int is_space(char x)  { return x == ' ' || x == '\t' || x == '\n'; }
 int is_parens(char x) { return x == '(' || x == ')'; }
-Object *car(Object *x) { return x->value.pair.data; }
-Object *cdr(Object *x) { return x->value.pair.next; }
-static void gettoken() {
+
+Object *get_head(Object *x) { return x->value.list.head; }
+Object *get_tail(Object *x) { return x->value.list.tail; }
+
+static void read_token() {
   int index = 0;
   while(is_space(look)) { look = getchar(); }
   if (is_parens(look)) {
     token[index++] = look;  look = getchar();
   } else {
-    while(index < SYMBOL_MAX - 1 && look != EOF && !is_space(look) && !is_parens(look)) {
-      token[index++] = look;  look = getchar();
+    while( && look != EOF && !is_space(look) && !is_parens(look)) {
+      if (index < SYMBOL_MAX)
+        token[index++] = look;
+      look = getchar();
     }
   }
   token[index] = '\0';
 }
 
-#define e_true     cons(intern("quote"), cons(intern("t"), 0))
-#define e_false    0
-
-Object * cons(Object * _car, Object * _cdr) {
-  Object *_pair = calloc( 1, sizeof (Object) );
-  _pair->tag = _Pair;
-  _pair->value.pair.data = _car;
-  _pair->value.pair.next = _cdr;
-  return _pair;
+Object *new_list(Object *_car, Object *_cdr) {
+  Object *obj = calloc( 1, sizeof (Object) );
+  obj->tag = _List;
+  obj->value.list.head = _car;
+  obj->value.list.tail = _cdr;
+  return obj;
 }
 
-Object * newsymbol(char *str) {
+Object *new_symbol(char *str) {
   Object *obj = calloc( 1, sizeof (Object) );
   obj->tag = _Symbol;
   obj->value.string = strdup(str);
   return obj;
 }
 
-Object * newprimop( Object * (*fn) (Object *) ) {
-  Object * obj = calloc(1, sizeof (Object) );
+Object *new_primitive(Object *(*fn) (Object *) ) {
+  Object *obj = calloc(1, sizeof (Object) );
   obj->tag = _Primitive;
   obj->value.pfn = fn;
   return obj;
 }
 
-Object * newclosure( Object *params, Object *body, Object *env ) {
-  Object * obj = calloc(1, sizeof (Object) );
+Object *new_closure(Object *params, Object *body, Object *env ) {
+  Object *obj = calloc(1, sizeof (Object) );
   obj->tag = _Closure;
   obj->value.closure.params = params;
   obj->value.closure.body = body;
@@ -75,28 +83,28 @@ Object * newclosure( Object *params, Object *body, Object *env ) {
   return obj;
 }
 
-Object * intern(char *sym) {
-  Object *_pair = symbols;
-  for ( ; _pair ; _pair = cdr(_pair))
-    if (strncmp(sym, car(_pair)->value.string, SYMBOL_MAX)==0)
-      return car(_pair);
-  symbols = cons(newsymbol(sym), symbols);
-  return car(symbols);
+Object *get_symbol(char *str) {
+  Object *list = symbols;
+  for (; list; list = get_tail(list))
+    if (strncmp(str, get_head(list)->value.string, SYMBOL_MAX)==0)
+      return get_head(list);
+  symbols = new_list(new_symbol(str), symbols);
+  return get_head(symbols);
 }
 
-Object * getlist();
+Object *read_list();
 
-Object * getobj() {
-  if (token[0] == '(') return getlist();
-  return intern(token);
+Object *read_obj() {
+  if (token[0] == '(') return read_list();
+  return get_symbol(token);
 }
 
-Object * getlist() {
+Object *read_list() {
   Object *tmp;
-  gettoken();
+  read_token();
   if (token[0] == ')') return (Object *) NULL;
-  tmp = getobj();
-  return cons(tmp, getlist());
+  tmp = read_obj();
+  return new_list(tmp, read_list());
 }
 
 void print_obj(Object *ob, int head_of_list) {
@@ -104,19 +112,19 @@ void print_obj(Object *ob, int head_of_list) {
     printf("null");
   } else if (ob->tag == _Symbol) {
     printf("%s", ob->value.string);
-  } else if (ob->tag == _Pair) {
+  } else if (ob->tag == _List) {
     if (head_of_list) printf("(");
-    print_obj(car(ob), 1);
-    if (cdr(ob) == 0) {
+    print_obj(get_head(ob), 1);
+    if (get_tail(ob) == 0) {
       printf(")");
     }
-    else if(cdr(ob)->tag == _Pair) {
+    else if(get_tail(ob)->tag == _List) {
       printf(" ");
-      print_obj(cdr(ob), 0);
+      print_obj(get_tail(ob), 0);
     }
     else {
       printf(" . ");
-      print_obj(cdr(ob), 0);
+      print_obj(get_tail(ob), 0);
       printf(")");
     }
   } else if (ob->tag == _Closure) {
@@ -124,81 +132,85 @@ void print_obj(Object *ob, int head_of_list) {
   }
 }
 
-Object *fcons(Object *a)    {  return cons(car(a), car(cdr(a)));  }
-Object *fcar(Object *a)     {  return car(car(a));  }
-Object *fcdr(Object *a)     {  return cdr(car(a));  }
-Object *feq(Object *a)      {  return car(a) == car(cdr(a)) ? (Object *) e_true : e_false;  }
-Object *fpair(Object *a)    {  return car(a)->tag == _Pair  ? e_true : e_false;  }
-Object *fatom(Object *a)    {  return car(a)->tag == _Symbol  ? e_true : e_false;  }
-Object *fnull(Object *a)    {  return car(a) == 0           ? e_true : e_false; }
-Object *freadobj(Object *a) {  look = getchar(); gettoken(); return getobj();  }
-Object *fwriteobj(Object *a){  print_obj(car(a), 1); puts(""); return e_true;  }
+Object *fn_list(Object *a)      { return new_list(get_head(a), get_head(get_tail(a))); }
+Object *fn_head(Object *a)      { return get_head(get_head(a)); }
+Object *fn_tail(Object *a)      { return get_tail(get_head(a)); }
+Object *fn_is_eq(Object *a)     { return get_head(a) == get_head(get_tail(a)) ? (Object *) exp_true : exp_false; }
+Object *fn_is_list(Object *a)   { return get_head(a)->tag == _List ? exp_true : exp_false; }
+Object *fn_is_symbol(Object *a) { return get_head(a)->tag == _Symbol ? exp_true : exp_false; }
+Object *fn_is_null(Object *a)   { return get_head(a) == 0 ? exp_true : exp_false; }
+Object *fn_read(Object *a)      { look = getchar(); read_token(); return read_obj(); }
+Object *fn_write(Object *a)     { print_obj(get_head(a), 1); puts(""); return exp_true; }
 
-Object * eval(Object *exp, Object *env);
+Object *eval(Object *exp, Object *env);
 
-Object * evlist(Object *list, Object *env) {
+Object *eval_list(Object *list, Object *env) {
   /* http://cslibrary.stanford.edu/105/LinkedListProblems.pdf */
   Object *head = 0, **args = &head;
-  for ( ; list ; list = cdr(list) ) {
-    *args = cons( eval(car(list), env) , 0);
-    args = &( (Object *) *args )->value.pair.next;
+  for (; list; list = get_tail(list)) {
+    *args = new_list(eval(get_head(list), env) , 0);
+    args = &((Object *) *args)->value.list.tail;
   }
   return head;
 }
 
-Object * evlist_bind_append(Object *list, Object *names, Object *env, Object *tail) {
+Object *eval_list_bind_append(Object *list, Object *names, Object *env, Object *tail) {
   Object *head = tail, **args = &head;
-  for ( ; list ; list = cdr(list), names = cdr(names) ) {
-    *args = cons( cons(car(names), cons(eval(car(list), env), 0))
-		, tail);
-    args = &( (Object *) *args )->value.pair.next;
+  for (; list; list = get_tail(list), names = get_tail(names)) {
+    *args = new_list(new_list(get_head(names), new_list(eval(get_head(list), env), 0)), tail);
+    args = &( (Object *) *args )->value.list.tail;
   }
   return head;
 }
 
-Object * evcond(Object *exp, Object *env) {
+Object *eval_cond(Object *exp, Object *env) {
   if (exp == 0) {
     return 0;
-  } else if (eval (car(car(exp)), env)) {
-    return eval(car(cdr(car(exp))), env);
+  } else if (eval (get_head(get_head(exp)), env)) {
+    return eval(get_head(get_tail(get_head(exp))), env);
   } else {
-    return evcond(cdr(exp), env);
+    return eval_cond(get_tail(exp), env);
   }
 }
 
-Object * apply_primitive(Object *primptr, Object *args) {
-  return primptr->value.pfn (args);
+Object *apply_primitive(Object *func, Object *args) {
+  return func->value.pfn(args);
 }
 
-Object * eval(Object *exp, Object *env) {
+Object *eval(Object *exp, Object *env) {
   if (exp->tag == _Symbol ) {
-    for ( ; env != 0; env = cdr(env) ) {
-      if (exp == car(car(env))) return car(cdr(car(env)));
+    for ( ; env != 0; env = get_tail(env) ) {
+      if (exp == get_head(get_head(env))) return get_head(get_tail(get_head(env)));
     }
     puts("unbound variable");
     return 0;
-  } else if (exp->tag == _Pair) {
-    Object *func = 0;
-    if ( car (exp) ->tag == _Symbol) { /* special forms */
-      if (car(exp) == intern("quote")) {
-        return car(cdr(exp));
-      } else if (car(exp) == intern("cond")) {
-        return evcond(cdr(exp), env);
-      } else if (car(exp) == intern("lambda") || car(exp) == intern("λ")) {
-        return newclosure(car(cdr(exp)), car(cdr(cdr(exp))), env);
-      } else if (car(exp) == intern("apply")) { /* apply function to list */
-        Object *args = evlist (cdr(cdr(exp)), env);
-        args = car(args); /* assumes one argument and that it is a list */
-        return apply_primitive( eval(car(cdr(exp)), env), args);
+  } else if (exp->tag == _List) {
+    Object *func;
+    if (get_head(exp)->tag == _Symbol) { /* special forms */
+      if (get_head(exp) == sym_quote) {
+        return get_head(get_tail(exp));
+      } else if (get_head(exp) == sym_if) {
+        return eval_cond(get_tail(exp), env);
+      } else if (get_head(exp) == sym_lambda) {
+        return new_closure(get_head(get_tail(exp)), get_head(get_tail(get_tail(exp))), env);
+      } else if (get_head(exp) == sym_apply) { /* apply function to list */
+        Object *args = eval_list (get_tail(get_tail(exp)), env);
+        args = get_head(args); /* assumes one argument and that it is a list */
+        return apply_primitive(eval(get_head(get_tail(exp)), env), args);
       }
     }
     /* function call */
-    func = eval (car(exp), env);
-    if (func && func->tag == _Closure) {
-      env = evlist_bind_append(cdr(exp), func->value.closure.params, env, func->value.closure.env);
-      return eval( func->value.closure.body, env );
-    } else if (func) { /* built-in primitive */
-      return apply_primitive(func, evlist(cdr(exp), env));
+    func = eval(get_head(exp), env);
+    if (func) { 
+      if (func->tag == _Closure) {
+        env = eval_list_bind_append(get_tail(exp), func->value.closure.params, env, func->value.closure.env);
+        return eval(func->value.closure.body, env );
+      } else if (func->tag == _Primitive) { /* built-in primitive */
+        return apply_primitive(func, eval_list(get_tail(exp), env));
+      } else {
+        puts("invalid function");
+        return 0;
+      }
     } else {
       puts("invalid application");
       return 0;
@@ -209,19 +221,21 @@ Object * eval(Object *exp, Object *env) {
 }
 
 int main(int argc, char *argv[]) {
-  Object *env = cons (cons(intern("car"), cons(newprimop(fcar), 0)),
-              cons (cons(intern("cdr"), cons(newprimop(fcdr), 0)),
-              cons (cons(intern("cons"), cons(newprimop(fcons), 0)),
-              cons (cons(intern("eq?"), cons(newprimop(feq), 0)),
-              cons (cons(intern("pair?"), cons(newprimop(fpair), 0)),
-              cons (cons(intern("symbol?"), cons(newprimop(fatom), 0)),
-              cons (cons(intern("null?"), cons(newprimop(fnull), 0)),
-              cons (cons(intern("read"), cons(newprimop(freadobj), 0)),
-              cons (cons(intern("write"), cons(newprimop(fwriteobj), 0)),
-              cons (cons(intern("null"), cons(0,0)), 0))))))))));
+  Object *env = new_list(new_list(get_symbol("head"), new_list(new_primitive(fn_head), 0)),
+                new_list(new_list(get_symbol("tail"), new_list(new_primitive(fn_tail), 0)),
+                new_list(new_list(get_symbol("list"), new_list(new_primitive(fn_list), 0)),
+                new_list(new_list(get_symbol("eq?"), new_list(new_primitive(fn_is_eq), 0)),
+                new_list(new_list(get_symbol("list?"), new_list(new_primitive(fn_is_list), 0)),
+                new_list(new_list(get_symbol("symbol?"), new_list(new_primitive(fn_is_symbol), 0)),
+                new_list(new_list(get_symbol("null?"), new_list(new_primitive(fn_is_null), 0)),
+                new_list(new_list(get_symbol("read"), new_list(new_primitive(fn_read), 0)),
+                new_list(new_list(get_symbol("write"), new_list(new_primitive(fn_write), 0)),
+                new_list(new_list(get_symbol("null"), new_list(0,0)), 0))))))))));
+  sym_quote = get_symbol("quote"); sym_if = get_symbol("if"); sym_lambda = get_symbol("lambda"); sym_apply = get_symbol("apply");
+  exp_true = new_list(get_symbol("quote"), new_list(get_symbol("t"), 0));
   look = getchar();
-  gettoken();
-  print_obj( eval(getobj(), env), 1 );
+  read_token();
+  print_obj(eval(read_obj(), env), 1);
   printf("\n");
   return 0;
 }
